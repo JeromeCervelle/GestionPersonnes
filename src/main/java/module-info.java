150f34ce6a0576f70db6fa7f0be9efafd8444525
JeromeCervelle <112790971+JeromeCervelle@users.noreply.github.com>
2023-10-14 19:03:05 +0200

    Modèle et Table view du projet GestionPersonnes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,6 @@ module fr.limoges.valadon.gestionpersonnes {
 
 
     opens fr.limoges.valadon.gestionpersonnes to javafx.fxml;
+    opens fr.limoges.valadon.gestionpersonnes.controller to javafx.fxml;
     exports fr.limoges.valadon.gestionpersonnes;
 }
