172bf2eb705d11bb4d9080f14a48a89ef4355b4c
JeromeCervelle <112790971+JeromeCervelle@users.noreply.github.com>
2023-10-15 09:52:35 +0200

    Création de EditPersonn et gestion de l'interface (boutons New, Edit et Delete)

--- src/main/java/fr/limoges/valadon/gestionpersonnes/MainApp.java
+++ src/main/java/fr/limoges/valadon/gestionpersonnes/MainApp.java
@@ -9,15 +9,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import fr.limoges.valadon.gestionpersonnes.model.Person;
+import fr.limoges.valadon.gestionpersonnes.controller.PersonEditDialogController;
 import fr.limoges.valadon.gestionpersonnes.controller.PersonOverviewController;
 
 public class MainApp extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
-
+    
     /**
      * The data as an observable list of Persons.
      */
@@ -40,7 +42,7 @@ public class MainApp extends Application {
     }
 
     /**
-     * Returns the data as an observable list of Persons.
+     * Returns the data as an observable list of Persons. 
      * @return
      */
     public ObservableList<Person> getPersonData() {
@@ -84,7 +86,7 @@ public class MainApp extends Application {
             // Load person overview.
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(MainApp.class.getResource("PersonOverview.fxml"));
-            AnchorPane personOverview = loader.load();
+            AnchorPane personOverview = (AnchorPane) loader.load();
 
             // Set person overview into the center of root layout.
             rootLayout.setCenter(personOverview);
@@ -95,6 +97,44 @@ public class MainApp extends Application {
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Opens a dialog to edit details for the specified person. If the user
+     * clicks OK, the changes are saved into the provided person object and true
+     * is returned.
+     * 
+     * @param person the person object to be edited
+     * @return true if the user clicked OK, false otherwise.
+     */
+    public boolean showPersonEditDialog(Person person) {
+        try {
+            // Load the fxml file and create a new stage for the popup dialog.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("PersonEditDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Create the dialog Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Set the person into the controller.
+            PersonEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(person);
+
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 
