6d120b298f6dc5a69dcbc24cba42492da06c260c
JeromeCervelle <112790971+JeromeCervelle@users.noreply.github.com>
2023-10-18 20:44:31 +0200

    Test de commit sur traduction IHM

--- src/main/java/fr/limoges/valadon/gestionpersonnes/MainApp.java
+++ src/main/java/fr/limoges/valadon/gestionpersonnes/MainApp.java
@@ -50,7 +50,7 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("AddressApp");
+        this.primaryStage.setTitle("Gestion des personnes");
 
         initRootLayout();
 
